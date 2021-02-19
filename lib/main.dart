be7f53a1f84d091ab3f0e6df432db55da7d02c52
Novjean J Kannathara <novjeanj@gmail.com>
2021-02-19 12:12:48 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // home is the core widget
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is the default text!'),
+      ),
+    );
+  }
+}
