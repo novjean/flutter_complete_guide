5d16143f0ead32081e02da1719dd014213d399ca
Novjean J Kannathara <novjeanj@gmail.com>
2021-02-22 15:26:33 +0530

    styling custom widget

--- lib/question.dart
+++ lib/question.dart
@@ -8,6 +8,14 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity, // as much as size as it can get
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
