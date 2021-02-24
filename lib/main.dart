09de951253d65b0a034f1d65be49dd489f2fa5c4
Novjean J Kannathara <novjeanj@gmail.com>
2021-02-24 15:03:16 +0530

    reset quiz

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,13 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
@@ -76,7 +83,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: _questionIndex < _questions.length
             ? Quiz(_answerQuestion, _questions, _questionIndex)
-            : Result(_totalScore),
+            : Result(_resetQuiz, _totalScore),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resetHandler, this.resultScore);
 
-  String get resultPhrase{
+  String get resultPhrase {
     var resultText = 'You did it!';
-    if(resultScore<=8){
+    if (resultScore <= 8) {
       resultText = 'You are awesome and innocent!';
-    } else if (resultScore<=12){
+    } else if (resultScore <= 12) {
       resultText = 'Pretty likeable!';
-    } else if (resultScore<=16){
+    } else if (resultScore <= 16) {
       resultText = 'You are ... strange?';
     } else {
       resultText = 'You are so bad!';
@@ -22,9 +23,22 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            onPressed: resetHandler,
+            child: (Text('Restart Quiz!')),
+            textColor: Colors.blue,
+          )
+        ],
       ),
     );
   }
